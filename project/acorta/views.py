ae3db7355ff385cfcc7a270736ae3839c382e55b
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha27.aulas.gsyc.urjc.es>
2018-04-09 18:48:22 +0200

    REdirection implemented with HTTPResponseRedirect

--- project/acorta/views.py
+++ project/acorta/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from .models import URL
 
 # Create your views here.
 
@@ -7,8 +10,9 @@ def barra(request):
     return(HttpResponse('Hola'))
 
 def numero(request, num):
-    return(HttpResponse('Adios'))
+    redirect_url = URL.objects.get(id=str(num))
+    return(HttpResponseRedirect(redirect_url))
 
 def notfound(request):
-    return(HttpResponse('<h1>Usage error</h1><br>' + 
+    return(HttpResponse('<h1>Usage error</h1><br>' +
                         'Try (server)/ or (server)/number'))
