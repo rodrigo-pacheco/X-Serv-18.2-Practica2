a4af504a83d699ec50507ad1c6337cf6fd5e25d9
Rodrigo Pacheco Martinez Atienza <rpacheco@beta14.aulas.gsyc.urjc.es>
2018-04-10 12:22:03 +0200

    Exception added

--- project/acorta/views.py
+++ project/acorta/views.py
@@ -20,7 +20,8 @@ from .models import URL
 USAGE_ERROR = """<h1>Usage error</h1><br>
               Try (server)/ [GET/POST] or
               (server)/number [GET] or
-              (server)/admin/ [GET]"""
+              (server)/admin/ [GET]
+              <p><a href=/>Back to home page</a></p>"""
 
 LINK = """<a href=/{}>{}</a>"""
 
@@ -54,22 +55,26 @@ def barra(request):
             new_url = URL(url=correct_url(request.POST["URL"]))
             new_url.save()
         except IntegrityError:
-            body = ("<html><body><h1>URL already added: </h1>"
-                    "<p><a href=/>Back to start page</a></p>")
+            body = ("<html><body><h1>URL already added</h1>"
+                    "<p><a href=/>Back to home page</a></p>")
             return(HttpResponse(body))
         body = ("<html><body><h1>Shortened URL: </h1>" +
                # "<p>"" + LINK{str(new_url.id), str(new_url.id)} +
                "<p><a href=/" + str(new_url.id) + ">" + str(new_url.id) + "</a>"
                " > " + "<a href=" + new_url.url + ">" + new_url.url + "</a></p>"
-               "<p><a href=/>Back to start page</a></p>" +
+               "<p><a href=/>Back to home page</a></p>" +
                "</body></html>")
         return(HttpResponse(body))
     else:
         return(HttpResponseNotFound(USAGE_ERROR))
 
 def numero(request, num):
-    redirect_url = URL.objects.get(id=str(num))
-    return(HttpResponseRedirect(redirect_url))
+    try:
+        redirect_url = URL.objects.get(id=str(num))
+        return(HttpResponseRedirect(redirect_url))
+    except URL.DoesNotExist:
+        return(HttpResponseNotFound("<html><body><h1>Number not in use</h1>"
+                                    "<p><a href=/>Back to home page</a></p>"))
 
 def notfound(request):
     return(HttpResponseNotFound(USAGE_ERROR))
