396b3bfad81f30a51075977ff920424faa7ba950
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha28.aulas.gsyc.urjc.es>
2018-04-10 09:44:40 +0200

    GET performing correctly. Changes to USAGE_ERROR. POST detected and csrf exemption implemented

--- project/acorta/views.py
+++ project/acorta/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import URL
+
+USAGE_ERROR = """<h1>Usage error</h1><br>
+              Try (server)/ [GET/POST] or
+              (server)/number [GET]"""
 
 # Create your views here.
 def current_url_links():
@@ -13,7 +18,7 @@ def current_url_links():
                       "-- " + "<a href=" + url.url + ">" + url.url + "</a></p>")
     return(html_code)
 
-
+@csrf_exempt
 def barra(request):
     if request.method == 'GET':
         return(HttpResponse("<html><body><h1>" +
@@ -22,11 +27,14 @@ def barra(request):
                 <input type="submit" value="Submit"></form>""" +
                 current_url_links() +
                 "</body></html>"))
+    elif request.method == 'POST':
+        return(HttpResponse('POST'))
+    else:
+        return(HttpResponse(USAGE_ERROR))
 
 def numero(request, num):
     redirect_url = URL.objects.get(id=str(num))
     return(HttpResponseRedirect(redirect_url))
 
 def notfound(request):
-    return(HttpResponse('<h1>Usage error</h1><br>' +
-                        'Try (server)/ or (server)/number'))
+    return(HttpResponse(USAGE_ERROR))
